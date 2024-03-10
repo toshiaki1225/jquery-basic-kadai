f788008d2a5ddbd1298b7c884ef10e5d1839c178
toshiaki1225 <125113596+toshiaki1225@users.noreply.github.com>
2024-03-11 05:39:27 +0900

    kadai_004をアップロード
    kadai_004をアップロードしました。

---
+++ kadai_004/event.js
@@ -0,0 +1,11 @@
+$(function(){
+  $(window).on('load',()=>{
+    console.log('loadイベントが発生しました。');
+  });
+});
+
+$(function(){
+  $(window).on('scroll',()=>{
+    console.log('scrollイベントが発生しました。');
+  });
+});
